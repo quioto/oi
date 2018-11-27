81c1d18821bf971807bf38995a4fe20960b77383
quioto <42048936+quioto@users.noreply.github.com>
2018-11-27 13:33:07 -0200

    Update Principal.py

--- trabfinal/Principal.py
+++ trabfinal/Principal.py
@@ -3,19 +3,5 @@ from carro import Carro
 from Vendedor import Vendedor
 from Comprador import Comprador
 from controle import Controle
-#
-# v12 = Concessionaria('V12 Motors')
-# up = Carro('UP', 'VW', 2018, 'Novo', 20000, 'JKJ-3445')
-# onix = Carro('Ônix', 'GM', 2018, 'Novo', 20500, 'JJJ-9986')
-# v12.add_carro(up)
-# v12.add_carro(onix)
-# for c in v12.get_carros():
-#     print(c.get_dados())
-#
-# vend1 = Vendedor('Jônas', '09674342165', 56349)
-# comp1 = Comprador('João', '25678490283')
-#
-# up.vender(comp1, vend1)
-# print(up.get_venda())
-# v12.save_cars()
+
 c = Controle()
